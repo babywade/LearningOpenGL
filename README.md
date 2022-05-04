b2818701bffccef6dd6d66416f9391ac1854d644
babywade <yixiaofei@stu.xjtu.edu.cn>
2022-05-04 15:55:21 +0800

    vscode+cmake+msvc的opengl工程初始化配置及DEMO示例

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LearningOpenGL
+
 学习OpenGL的历程
+
+2022/4/17 开始入坑之旅~
+
+参考教程：https://learnopengl-cn.github.io/
+
+使用的OpenGL库：GLFW 3.3.7预编译x64版本
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/学习OpenGL/LearningOpenGL
+# Build directory: D:/学习OpenGL/LearningOpenGL/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
